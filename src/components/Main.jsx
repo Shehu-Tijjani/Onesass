f43bf42288ea98751d2f94ef43dbbecff0b183fa
CodeBroTee <95186415+Shehu-Tijjani@users.noreply.github.com>
2026-07-31 19:31:41 +0100

    Fix import path for Testimonials

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 import Brands from "./Brands";
 import ProductPlan from "./ProductPlan";
 import Features from "./Features";
-import Testimonials from "./testimonials";
+import Testimonials from "./Testimonials";
 import Newsletter from "./Newsletter";
 
 function Main() {
