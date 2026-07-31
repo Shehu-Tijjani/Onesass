a3cccfb767fe214b8c1e59c920b0818ec96b31b6
CodeBroTee <95186415+Shehu-Tijjani@users.noreply.github.com>
2026-07-31 19:40:27 +0100

    Adjust max height of header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header
       id="home"
-      className="bg-hero-gradient h-screen max-h-170 w-full min-h-max flex flex-col  relative overflow-hidden"
+      className="bg-hero-gradient h-screen max-h-160 w-full min-h-max flex flex-col  relative overflow-hidden"
     >
       <Navbar />
       <Hero />
